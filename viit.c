a8d934ffb662fdb91af1351be9f0bdacd8660f48
zacdogwater <zacsengines@gmail.com>
2025-09-21 20:45:41 -0600

    half block chars and hide cursor

--- viit.c
+++ viit.c
@@ -19,22 +19,28 @@ int main(int argc, char **argv) {
 	int iwidth, iheight, num_channels;	
 	unsigned char *img_data = stbi_load(argv[1], &iwidth, &iheight, &num_channels, 0);
 
-	printf("\e[?1049h");
+	printf("\e[?1049h\e[?25l");
 	for (int y = 0; y < theight; y++) {
 		printf("\e[%d;0H", y + 1);
 		for (int x = 0; x < twidth; x++) {
-			int imgy = y * iheight / theight;
 			int imgx = x * iwidth / twidth;
-			unsigned char *off = img_data + (imgy * iwidth + imgx) * num_channels;
-			int r = off[0];
-			int g = off[1];
-			int b = off[2];
-			printf("\x1b[48;2;%d;%d;%dm ", r, g, b);
+			int timgy = (y * 2 + 0) * iheight / (theight * 2);
+			int bimgy = (y * 2 + 1) * iheight / (theight * 2);
+			unsigned char *offt = img_data + (timgy * iwidth + imgx) * num_channels;
+			unsigned char *offb = img_data + (bimgy * iwidth + imgx) * num_channels;
+			int rt = offt[0];
+			int gt = offt[1];
+			int bt = offt[2];
+			int rb = offb[0];
+			int gb = offb[1];
+			int bb = offb[2];
+			printf("\e[48;2;%d;%d;%dm\e[38;2;%d;%d;%dm\u2584", rt, gt, bt, rb, gb, bb);
 		}
 	}
 
 	getchar();
-	printf("\e[?1049l");
+	printf("\e[?1049l\e[?25h");
 
 	stbi_image_free(img_data);
 }
+
